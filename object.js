c47ed7ff0e788454fd5b3ccfb4a7477078644c80
AjithAmin004 <ajithsowda33@gmail.com>
2022-03-08 22:22:28 +0530

    Captain America obj is created

--- object.js
+++ object.js
@@ -8,3 +8,18 @@ let objajith = {
      LName : "Amin"
 };
 console.log(objajith);
+
+
+let capAmerica = {
+    name:"Captain America",
+    age:87,
+    friends:["Bucky","Tony","Natasha","Falcon"],
+    address:{
+        city:"Kundapura",
+        State:"Karnataka"
+    },
+    sayHi:function(){
+        console.log("Captain America Say Hi to Ajith");
+    }
+}
+console.log(capAmerica);
